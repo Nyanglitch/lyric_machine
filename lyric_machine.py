47ec2438b3e4bd33b362692f021790ac52372a0b
Nyanglitch <kittysaymeow2000@gmail.com>
2020-06-19 19:05:59 -0700

    Lyric Machine v2.0 -- improved input, output, and connection.

--- lyric_machine.py
+++ lyric_machine.py
@@ -1,17 +1,56 @@
 import os
 from sys import argv
+import urllib.error
 import urllib.request
 from bs4 import BeautifulSoup
+import os.path
+from os import path
 
+
+# print("Pass singer and song name. You may use quotes (if name contains spaces), apostrophes, and any case.\nPerfect example if in doubt: acdc highwaytohell")
 file, singer, song = argv
+# singer = input("Singer: ")
+# song = input("Song name: ")
 
-# if singer and song == 'help':
-# 	print('Help: ')
+if singer and song == 'help':
+	print('Help: Pass singer and song name. You may use quotes (if name contains spaces), apostrophes, and any case.\nPerfect example if in doubt: acdc highwaytohell\nThe program will make a separate folder in your current directory for outputs.')
+	exit()
 
-# song = song.replace('"', '')
+# make a dir // check an existing dir
 
-url = 'https://www.azlyrics.com/lyrics/' + singer + '/' + song + '.html'
-html = urllib.request.urlopen(url).read()
+if path.exists('lyric_machine') == False:
+	os.mkdir("lyric_machine")
+
+# maybe make it a function?
+
+singer_clean = singer.lower()
+singer_clean = singer_clean.replace(' ', '')
+singer_clean = singer_clean.replace("'", "")
+singer_clean = singer_clean.replace("/", "")
+song_clean = song.replace(' ', '')
+song_clean = song_clean.replace('"', '')
+song_clean = song_clean.replace("'", "")
+song_clean = song_clean.lower()
+# song_filename = song.replace(' ', '_')
+song_filename = song.replace('"', '')
+
+url = 'https://www.azlyrics.com/lyrics/' + singer_clean + '/' + song_clean + '.html'
+
+try:
+    conn = urllib.request.urlopen(url)
+except urllib.error.HTTPError as e:
+    print('HTTPError: {}'.format(e.code))
+    print('Page does not exist. Try different input or different song :(')
+    exit()
+except urllib.error.URLError as e:
+    print('URLError: {}'.format(e.reason))
+    print('Page does not exist. Try different input or different song :(')
+    exit()
+else:
+    html = urllib.request.urlopen(url).read()
+
+
+
 soup = BeautifulSoup(html, 'html.parser')
 
 # kill all script and style elements
@@ -28,9 +67,6 @@ chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
 # drop blank lines
 text = '\n'.join(chunk for chunk in chunks if chunk)
 
-# text.readline(range([1, 5]))
-
-# print(text)
 
 # adding everything to the text file
 
@@ -41,28 +77,44 @@ out_file.close()
 # cleaning up the output
 
 out_file = open('messy_output.txt', 'r')
-clear_out_file = open(singer + ' -- ' + song + '.txt', 'w')
+clear_out_file = open('lyric_machine/' + singer + ' -- ' + song_filename + '.txt', 'w')
 
 whole_content = ''
 
 cont = out_file.readlines() 
-# type(cont) 
 for i in range(0, len(cont)): 
     if(i in range(0, 30)):
         pass
-    # elif i == 31:
-    # 	beginning, sep, rest = cont[i].partition(' Lyrics')
-    # 	whole_content = whole_content + beginning
     else: 
         whole_content = whole_content + cont[i]
 
-# additions
+# cleanup
 
 head, sep, tail = whole_content.partition('Submit Corrections')
-
 clear_out_file.write(head)
+
+remove_lyrics = " Lyrics"
+head = head.replace(remove_lyrics, '')
+
 print(head)
 
 clear_out_file.close()
 out_file.close()
 os.remove('messy_output.txt')
+
+# save or not?
+
+def save_submit():
+	save_or_not = input("Keep lyrics as a text file? (y/n): ").lower()
+	if save_or_not == 'n':
+		os.remove('lyric_machine/' + singer + ' -- ' + song_filename + '.txt')
+	elif save_or_not == 'y':
+		exit()
+	elif save_or_not == 'u so cool!':
+		print('Aww! Thank you!')
+		save_submit()
+	else:
+		print("Letter not supported.")
+		save_submit()
+
+save_submit()
