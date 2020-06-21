6f6c92bfe97b4f5af5f275cedb96a164c8925593
Nyanglitch <kittysaymeow2000@gmail.com>
2020-06-20 20:01:32 -0700

    Lyric Machine 2.13 -- regexp is back.
    Mistake undersrtood shortly after committing the previous way.

--- lyric_machine.py
+++ lyric_machine.py
@@ -6,6 +6,7 @@ import urllib.request
 from bs4 import BeautifulSoup
 import os.path
 from os import path
+import re
 
 error_arg1 = 'No arguments passed. Try "help" argument.'
 error_arg2 = 'Too many arguments. Try "help" once.'
@@ -55,8 +56,8 @@ if path.exists('lyric_machine') == False:
 
 song_filename = song.replace('"', '')
 singer_filename = singer.replace('"', '')
-song_clean = ''.join(e for e in song_filename if e.isalnum()).lower()
-singer_clean = ''.join(e for e in singer_filename if e.isalnum()).lower()
+song_clean = re.sub("[^a-z0-9A-Z]", '', song_filename).replace(" ", '').lower()
+singer_clean = re.sub("[^a-z0-9A-Z]", '', singer_filename).replace(" ", '').lower()
 
 # the path concatenation
 
